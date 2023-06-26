a289264a5a25dd90a445b9b4979ac89b55d35752
DJ <97788700+DJMONSTER0212@users.noreply.github.com>
2023-06-26 16:56:15 +0530

    created feedback part

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -21,10 +21,10 @@ const Hero = () => {
           <ComputersCanvas/>
           <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
             <a href='#about'>
-              <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+              <div className='h-[35px] w-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
                 <motion.div 
                   animate={{
-                    y:[0,24,0]
+                    x:[-24,24,-24]
                   }}
                   transition={{
                     duration:1.5,
